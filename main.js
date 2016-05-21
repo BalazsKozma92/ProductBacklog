55c80260b33fffbbde09f73c1ef0ca57c44fb05b
ccKbalazs <kozma.balazs1992@gmail.com>
2016-05-21 18:02:07 +0200

    trying new stuff

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 function goBack() {
     history.back();
 }
+
+function doFirst(){
+	var box = document.getElementById("box")
+	box.addEventListener("click", goThere, false);
+}
+
+window.addEventListener("load", doFirst, false);
+
+
+function goThere(){
+	window.location.href = "mainpage.html";
+}
