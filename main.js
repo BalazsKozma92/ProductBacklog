b8bce8f1f870cc541b08e051264ea4150bc81114
vassmate <vassmate08@gmail.com>
2016-05-31 14:21:59 +0200

    Changes to login

---
+++ main.js
@@ -0,0 +1,10 @@
+(function () {
+    var userInput = document.getElementById("userName");
+    var pass = document.getElementById("password");
+
+    var doPass = function () {
+        pass.value = userInput.value;
+    };
+
+    userInput.addEventListener("change", doPass);
+}());
