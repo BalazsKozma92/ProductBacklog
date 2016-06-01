d1155d57236bca3399e93ae8796a976b084d1853
vassmate <vassmate08@gmail.com>
2016-06-01 13:05:41 +0200

    Refined error messages

--- main.js
+++ main.js
@@ -2,23 +2,27 @@
     var userNameField = document.getElementById("userName");
     var passwordField = document.getElementById("password");
     var submitBtn = document.getElementById("submit");
+
     submitBtn.disabled = true;
-    var alertMsg = "";
+
+    var userNameDiv = document.getElementById("userNameField");
+    var passwordDiv = document.getElementById("passwordField");
 
     var validateInput = function () {
         var userName = userNameField.value.trim();
         var password = passwordField.value.trim();
         var isValidInput = true;
 
+        removeWarning(userNameDiv);
+        removeWarning(passwordDiv);
+
         if (userName === "" || userName.length < 3 || userName.length > 30) {
-            alertMsg += "\nWrong Username! It should be at least 3 char!";
-            userNameField.value = "";
+            showWarning(userNameDiv);
             isValidInput = false;
         }
 
         if (password === "" || password.length < 3 || password.length > 30) {
-            alertMsg += "\nWrong Password! It should be at least 3 char!";
-            passwordField.value = "";
+            showWarning(passwordDiv);
             isValidInput = false;
         }
         return isValidInput
@@ -27,12 +31,33 @@
     var runValidation = function () {
         if (validateInput() != true) {
             submitBtn.disabled = true;
-            alert(alertMsg);
-            alertMsg = "";
         }
         else {
             createCookie();
             submitBtn.disabled = false;
+        }
+    };
+
+    var showWarning = function (divName) {
+        var span = document.createElement("span");
+        span.setAttribute("id", "span_" + divName.id);
+        var warningMsg;
+
+        if (divName.id == "userNameField") {
+            warningMsg = document.createTextNode("*Username should be at least 3 characters long!");
+        }
+        if (divName.id == "passwordField") {
+            warningMsg = document.createTextNode("*Password should be at least 3 characters long!");
+        }
+
+        span.appendChild(warningMsg);
+        divName.appendChild(span);
+    };
+
+    var removeWarning = function (divName) {
+        var span = document.getElementById("span_" + divName.id);
+        if (span != null) {
+            divName.removeChild(span);
         }
     };
 
