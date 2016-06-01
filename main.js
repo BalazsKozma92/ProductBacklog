f7e2be928db6b4942091c324e96693c9b862dd2f
Kozma Balazs <kozma.balazs1992@gmail.com>
2016-06-01 12:19:44 +0200

    !important changes

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ $(function(){
 function workBelt(){
 	$('.menuUnit').click(function(){
 		$('.work-belt').css('left','-100%');
+		$('.work-wrap').show();
 	});
 	
 	$('.work-return').click(function(){
 		$('.work-belt').css('left','0%');
+		$('.work-wrap').hide(400);
 	});
 }
